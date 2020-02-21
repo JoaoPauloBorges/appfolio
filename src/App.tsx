855625c081b2d37591a99c3cd3890f93558f6261
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2020-02-21 01:11:19 -0300

    adding materialUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,42 @@ import React, { useState } from "react";
 
 import Toolbar from "./components/Toolbar/Toolbar";
 import SideDrawer from "./components/SideDrawer/SideDrawer";
+import Button from '@material-ui/core/Button';
 import Backdrop from "./components/Backdrop/Backdrop";
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+type DrawerSide = 'left' | 'bottom';
 
 function App() {
   const [toggle, setToggle] = useState(false);
   const handleToggle = () => setToggle(!toggle);
   const deactiveDrawer = () => setToggle(false);
 
-  let backdrop;
-  if (toggle) {
-    backdrop = <Backdrop click={deactiveDrawer} />;
-  }
+  const [state, setState] = useState({ left: false, bottom: false });
+
+  let backdrop = toggle ? <Backdrop click={deactiveDrawer} /> : null; 
+
+  const toggleDrawer = (side: DrawerSide, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (event && event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift'))
+        return;
+      setState({ ...state, [side]: open });
+    };
 
   return (
     <div style={{ height: "100%" }} className="App">
       <Toolbar handleToggle={handleToggle} />
       <SideDrawer show={toggle} />;{backdrop}
+      <SwipeableDrawer
+        anchor="bottom"
+        open={state.bottom}
+        onClose={toggleDrawer('bottom', false)}
+        onOpen={toggleDrawer('bottom', true)}>
+        lalala
+      </SwipeableDrawer>
       <main style={{ marginTop: "74px" }}>
+        <Button onClick={toggleDrawer('bottom', true)}>Open Bottom</Button>
         <p> this is a page content </p>
       </main>
     </div>
--- src/components/SideDrawer/DrawerToggle.tsx
+++ src/components/SideDrawer/DrawerToggle.tsx
@@ -1,16 +1,41 @@
 import React from "react";
+import IconButton from '@material-ui/core/IconButton';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import "./DrawerToggle.css";
+
 interface Props {
-  click():  void;
+  click(): void;
 }
 
-const drawerToggle: React.FC<Props> = ({click}) => (
-  <button className="toggle-button" onClick={click}>
-    <div className="toggle-button__line" />
-    <div className="toggle-button__line" />
-    <div className="toggle-button__line" />
-  </button>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
 );
 
-export default drawerToggle;
+const DrawerToggle: React.FC<Props> = ({ click }) => {
+  const classes = useStyles();
+
+  return (
+    <div className="drawerWrapper">
+      <IconButton edge="start" className={classes.menuButton} onClick={click} color="inherit" aria-label="menu">
+        <div className="toggle-button">
+          <div className="toggle-button__line" />
+          <div className="toggle-button__line" />
+          <div className="toggle-button__line" />
+        </div>
+      </IconButton>
+    </div>
+  )
+};
+
+export default DrawerToggle;
